bf91964e789a02bda997a6b2a8f341d9f4a6b427
배석진 <bsj1044@naver.com>
2026-02-05 23:35:54 +0900

    #1 feat: JWT Access Token 검사, 파싱 유틸리티 구현

--- src/main/java/com/alogdalog/mallog/gateway/global/util/JwtTokenProviderImpl.java
+++ src/main/java/com/alogdalog/mallog/gateway/global/util/JwtTokenProviderImpl.java
@@ -1,16 +1,45 @@
 package com.alogdalog.mallog.gateway.global.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class JwtTokenProviderImpl implements JwtTokenProvider {
+
+    private final SecretKey secretKey;
+
+    public JwtTokenProviderImpl(@Value("${jwt.secret}") String secret) {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
     @Override
     public boolean validateToken(String token) {
-        return false;
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
     public Long getUserId(String token) {
-        return 0L;
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.get("sub", Long.class);
     }
 }
