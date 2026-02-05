3356b5399e2441dd6da98486c050cae0f0127417
배석진 <bsj1044@naver.com>
2026-02-05 18:24:12 +0900

    #1 init: initialize Gateway Service

---
+++ src/main/java/com/alogdalog/mallog/gateway/MalogApplication.java
@@ -0,0 +1,13 @@
+package com.alogdalog.mallog.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalogApplication.class, args);
+	}
+
+}
